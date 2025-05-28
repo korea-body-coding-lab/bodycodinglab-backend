5b405a13aefec01f0747c06e8909f4d18af7d144
leejunwoo24 <yusm8758@naver.com>
2025-05-28 17:35:01 +0900

    feat Member-entity 20250528

---
+++ src/main/java/com/project/bcl_back/entity/Member.java
@@ -0,0 +1,29 @@
+package com.project.bcl_back.entity;
+
+import com.project.bcl_back.common.enums.memberEnums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "members")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "member_address", nullable = false)
+    private String memberAddress;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    @Column(name = "is_approved", columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private Boolean isApproved;
+}
