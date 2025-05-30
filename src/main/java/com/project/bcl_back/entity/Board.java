53f222b4da76e0c6775bf7f37e431afd00078e6e
Yhs-DalC <scvthego@gmail.com>
2025-05-30 14:21:22 +0900

    refactor columnName

--- src/main/java/com/project/bcl_back/entity/Board.java
+++ src/main/java/com/project/bcl_back/entity/Board.java
@@ -27,22 +27,23 @@ public class Board {
     @OneToMany(mappedBy = "personal_community_board_categories", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Category> category;
 
-    @Column(nullable = false)
-    private String writerId;
+    @Column(name = "writer_id",nullable = false)
+    private Long writerId;
 
-    @Column(nullable = false)
+    @Column(name = "post_title",nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(name = "post_content",nullable = false)
     private String content;
 
-    @Column
-    private String imageUrl;
+    @Lob
+    @Column(name = "post_image", columnDefinition = "LONGBLOB")
+    private byte[] postImage;
 
-    @Column(nullable = false)
+    @Column(name = "created_at",nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
     @OneToMany(mappedBy = "personal_community_board_post", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
