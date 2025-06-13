ff39b294166504d2e1906b2ce9e1a4a9236ad17a
leejunwoo24 <yusm8758@naver.com>
2025-06-13 14:13:52 +0900

    refactor: matchserviceimpl

--- src/main/java/com/project/bcl_back/service/MatchService.java
+++ src/main/java/com/project/bcl_back/service/MatchService.java
@@ -1,15 +1,15 @@
-package com.project.bcl_back.service;
-
-import com.project.bcl_back.dto.ResponseDto;
-import com.project.bcl_back.dto.match.response.MemberMatchListResponseDto;
-import com.project.bcl_back.dto.match.response.TrainerMatchResponseDto;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public interface MatchService {
-    ResponseDto<TrainerMatchResponseDto> findMatchTrainer(Long userId);
-
-    ResponseDto<List<MemberMatchListResponseDto>> findMatchMemberList(Long userId);
-}
+//package com.project.bcl_back.service;
+//
+//import com.project.bcl_back.dto.ResponseDto;
+//import com.project.bcl_back.dto.match.response.MemberMatchListResponseDto;
+//import com.project.bcl_back.dto.match.response.TrainerMatchResponseDto;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public interface MatchService {
+//    ResponseDto<TrainerMatchResponseDto> findMatchTrainer(Long userId);
+//
+//    ResponseDto<List<MemberMatchListResponseDto>> findMatchMemberList(Long userId);
+//}
