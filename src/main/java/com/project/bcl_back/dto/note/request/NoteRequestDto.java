d33074c88a70cc8545fc56ac87b5c80077e9a17f
Yhs-DalC <scvthego@gmail.com>
2025-06-17 11:41:04 +0900

    refactor note

--- src/main/java/com/project/bcl_back/dto/note/request/NoteRequestDto.java
+++ src/main/java/com/project/bcl_back/dto/note/request/NoteRequestDto.java
@@ -2,6 +2,7 @@ package com.project.bcl_back.dto.note.request;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,8 @@ import lombok.NoArgsConstructor;
 public class NoteRequestDto {
     @NotBlank(message = "텍스트는 필수 입력 값입니다.")
     private String noteText;
-    @NotBlank(message = "받는 사람은 필수 입력 값입니다.")
+
+    private Long noteWriter;
+    @NotNull(message = "받는 사람은 필수 입력 값입니다.")
     private Long noteReceiver;
 }
