a49b7a87c941dac64a7cfe504a4a2405667772d5
Yhs-DalC <scvthego@gmail.com>
2025-06-24 17:22:34 +0900

    feat: add comment dto: createdAt

--- src/main/java/com/project/bcl_back/service/impl/CommentServiceImpl.java
+++ src/main/java/com/project/bcl_back/service/impl/CommentServiceImpl.java
@@ -18,8 +18,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.security.access.AccessDeniedException;
-
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
